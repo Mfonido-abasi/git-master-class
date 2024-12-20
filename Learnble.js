3ba966765cbe291d657a62af4f78dc13fea5a41d
ExceptionalM <ikonomfonido@gmail.com>
2024-12-20 21:49:11 +0100

    First ever commit, yayyy

---
+++ Learnble.js
@@ -0,0 +1,21 @@
+//"How old are you"
+// INPUT person's age
+// STORE person as age
+// IF age < 15
+    //OUTPUT you are eligible for a discount 
+//ELSE 
+    // OUTPUT you are not eligible for a discount 
+
+
+    let age = 15;
+    if (age < 15) {
+        console.log("Person is eligible for discount") ;
+    }
+    console.log("Thank you for patronising us")
+
+    let age = 13;
+    if (age < 15) {
+        console.log("Person is eligible for discount") ;
+    }
+    console.log("Thank you for patronising us");
+    
